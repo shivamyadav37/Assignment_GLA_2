fa00466ecd9a00457659b0f48874d17cc9fa2d7d
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-27 21:32:55 +0530

    Making Methods in MyBinarySearchTree.java

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -23,11 +23,6 @@ public class TreeNode {
     public void setData(int data) {
         this.data = data;
     }
-    public TreeNode(TreeNode left, TreeNode right, int data) {
-        this.left = left;
-        this.right = right;
-        this.data = data;
-    }
     public TreeNode getLeft() {
         return left;
     }
