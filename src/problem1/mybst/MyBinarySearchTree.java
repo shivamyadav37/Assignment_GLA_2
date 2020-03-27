e81aedbdccd59b9125c5f78effa763c7bb93f16e
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-27 21:34:58 +0530

    Update MyBinarySearchTree.java

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -14,63 +14,8 @@ public class MyBinarySearchTree {
         preOrderString = new StringBuilder();
     }
 
-    public static void preOrder(TreeNode root) {
-        if (root == null) {
-            return;
-        }
 
-        preOrder(root.getLeft());
-        preOrder(root.getRight());
-        preOrderString.append(root.getData() + " ");
-    }
 
-    public void add(final int value) {
-        this.root = addService(this.root, value);
-    }
 
-    public TreeNode addService(final TreeNode treeRoot, final int addingValue) {
-        if (treeRoot == null) {
-            return new TreeNode(addingValue);
-        }
-        if (addingValue < treeRoot.getData()) {
-            treeRoot.setLeft(addService(treeRoot.getLeft(), addingValue));
-        } else if (addingValue == treeRoot.getData()) {
-            return treeRoot;
-        } else {
-            treeRoot.setRight(addService(treeRoot.getRight(), addingValue));
-        }
 
-        return treeRoot;
-    }
-
-    public void postOrder(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-
-        postOrder(root.getLeft());
-        postOrder(root.getRight());
-        System.out.print(root.getData() + " ");
-    }
-
-    private void LeftNode(TreeNode root) {
-        if (root == null) return;
-        else {
-
-            if (root.getLeft() != null)
-                System.out.println(root.getLeft().getData());
-            else
-                count++;
-            LeftNode(root.getLeft());
-            LeftNode(root.getRight());
-        }
-    }
-
-    public void printLeftNode() {
-        LeftNode(root);
-    }
-
-    public void countNotLeft() {
-        System.out.println(count);
-    }
 }
