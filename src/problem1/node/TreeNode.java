474388b25973ba4a6dbe2601242a2301d044e0c9
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-27 21:26:46 +0530

    Making Methods in Node Class

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -20,6 +20,22 @@ public class TreeNode {
     public int getData(){
         return data;
     }
+    public TreeNode(TreeNode left, TreeNode right, int data) {
+        this.left = left;
+        this.right = right;
+        this.data = data;
+    }
 
+    public void setData(){
+        this.data = data;
 
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
 }
