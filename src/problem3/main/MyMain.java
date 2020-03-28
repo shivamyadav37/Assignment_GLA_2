94fe9b1726324413984967d73802351045063099
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-28 17:38:14 +0530

    MyMain.java Updates

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -1,7 +1,13 @@
-
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+import problem5.student.Student;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+
+    }
 
 }
