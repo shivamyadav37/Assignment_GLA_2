b80769c5861e8e941cc2405af49f2fe464ae3990
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-28 16:22:59 +0530

    Making MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -19,6 +19,10 @@ public class MyMain {
             }
             i--;
         }
+        System.out.println("All Left-children are :");
+        tree.printLeftNode();
+        System.out.println("Number of nodes doesn't have left child :");
+        tree.countNotLeft();
 
 
     }
