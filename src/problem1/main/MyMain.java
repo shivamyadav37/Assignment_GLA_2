5f938cd4811eb7562d4ac2459990349facd3ab43
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-28 16:26:31 +0530

    Making MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -9,8 +9,9 @@ public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree tree = new MyBinarySearchTree();
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input the Values-->");
         int i = scanner.nextInt();
-        System.out.println("Input the Values");
         while (i > 0) {
             try {
                 tree.add(scanner.nextInt());
