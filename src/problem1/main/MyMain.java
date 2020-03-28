afebb8da0503156a84c155d1908b3b747642287b
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-28 16:22:09 +0530

    Making Methods in MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -9,6 +9,17 @@ public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree tree = new MyBinarySearchTree();
         Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        System.out.println("Input the Values");
+        while (i > 0) {
+            try {
+                tree.add(scanner.nextInt());
+            } catch (Exception e) {
+                System.out.println("Error Occurred is " + e + " \n");
+            }
+            i--;
+        }
+
 
     }
 
