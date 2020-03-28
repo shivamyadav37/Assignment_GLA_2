bf45a8b3337478c32418a7c4f039353b409f91bd
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-28 16:19:34 +0530

    Making Methods in MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,7 +1,14 @@
 
 package problem1.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
     public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
 
     }
 
