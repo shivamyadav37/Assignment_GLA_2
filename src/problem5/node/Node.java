ef6581762f73911f0dc15cc2455e09b2194edc9c
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-28 16:54:30 +0530

    MyCircularQueue.java Updates

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,5 +1,15 @@
 
 package problem5.node;
 // to define node properties
+import problem5.student.Student;
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+
 }
