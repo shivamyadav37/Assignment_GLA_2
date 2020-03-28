2eec4107004cb66a266902dd3e8227b2055edffa
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-28 16:57:51 +0530

    Student.java Updates

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -2,4 +2,36 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    private int rollNumber;
+    private String Name;
+    private int backLogCounter;
+    private int appearingCounter;
+
+    public Student(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        Name = name;
+    }
+    public Student(int rollNumber, String name, int backLogCounter, int appearingCounter) {
+        this.rollNumber = rollNumber;
+        Name = name;
+        this.backLogCounter = backLogCounter;
+        this.appearingCounter = appearingCounter;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public int getBackLogCounter() {
+        return backLogCounter;
+    }
+
+    public int getAppearingCounter() {
+        return appearingCounter;
+    }
+
 }
