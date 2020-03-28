7603fffb50e937d5996e6d33123e30fcb2494417
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-28 17:33:01 +0530

    MyPriorityQueue.java Updates

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -4,4 +4,14 @@ package problem3.myqueue;
 import problem3.node.Node;
 
 public class MyPriorityQueue {
+    private Node rear;
+    private Node front;
+    private int size;
+
+    public MyPriorityQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+
 }
