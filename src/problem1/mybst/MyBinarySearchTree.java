0a11bb7ef8011f178f1a98836d2c426202bfa654
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-28 16:10:50 +0530

    Making Methods in MyBinarySearchTree.java

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -1,5 +1,18 @@
 package problem1.mybst;
 
+import problem1.node.TreeNode;
+
 public class MyBinarySearchTree {
+    public static StringBuilder preOrderString;
+    static int levels = 0;
+    static int max_level = 0;
+    public TreeNode root;
+    private int count = 0;
+    public MyBinarySearchTree() {
+        this.root = null;
+        preOrderString = new StringBuilder();
+    }
+
+
 
 }
