34d5e9c6c081e2678fdb035d4c8d93434348e464
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-03-28 17:30:45 +0530

    Node.java Updates

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -1,5 +1,27 @@
 
 package problem3.node;
+import problem5.student.Student;
 
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Node(Node next) {
+        this.next = next;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Node(Student student) {
+        this.student = student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+    public Student getData() {
+        return student;
+    }
 }
